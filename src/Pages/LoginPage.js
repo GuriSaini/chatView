e3ee32863452ba5f8dd4707022f77bb85e93ceef
Guri Saini <gurisaini@Guris-MacBook-Air.local>
2021-08-19 07:25:02 +0530

    view

---
+++ src/Pages/LoginPage.js
@@ -0,0 +1,32 @@
+function LoginPage() {
+    return (
+        <div className="card">
+        <div className="cardHeader">Login</div>
+        <div className="cardBody">
+          <div className="inputGroup">
+            <label htmlFor="email">Email</label>
+            <input
+              type="email"
+              name="email"
+              id="email"
+              placeholder="abc@example.com"
+              //ref={emailRef}
+            />
+          </div>
+          <div className="inputGroup">
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              name="password"
+              id="password"
+              placeholder="Your Password"
+              //ref={passwordRef}
+            />
+          </div>
+          <button >Login</button>
+        </div>
+      </div>
+    );
+  }
+  
+  export default LoginPage;
---
+++ src/Pages/RegisterPage.js
@@ -0,0 +1,7 @@
+function RegisterPage() {
+    return (
+      <div>Login</div>
+    );
+  }
+  
+  export default RegisterPage;
